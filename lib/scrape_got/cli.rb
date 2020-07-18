148cdf58d7380cb3111a58644d5db4322818265d
ByteSizeMagic <bytesizemagic@gmail.com>
2020-07-18 15:59:47 -0700

    added colorize

--- lib/scrape_got/cli.rb
+++ lib/scrape_got/cli.rb
@@ -1,38 +1,41 @@
+require 'colorize'
+
 module ScrapeGot
   class CLI
     def run
       system("clear")
       greeting
       Info.all_books
-      puts "___________________________\nPlease choose an option: "
+      puts "___________________________________\n".colorize(:light_yellow)
+      puts "Please choose an option: "
       @user_input = nil
       menu
       while @user_input != "exit"
         @user_input = gets.strip.downcase
           if @user_input == "1"
-            puts "List Books:"
+            puts "List Books:".colorize(:light_yellow)
             print_books
             display_menu_message
           elsif @user_input == "2"
-            puts "List Houses:"
+            puts "List Houses:".colorize(:light_yellow)
             print_houses
             display_menu_message
           else
             display_menu_message
           end
       end
-      binding.pry
+      #binding.pry
       end_program
     end
 
     def greeting 
-      puts "   ... Welcome to ... \nI scrape and I know things!\n   "
+      puts "     ... Welcome to ...  \n  I scrape and I know things!\n   ".colorize(:light_yellow)
     end
 
     def menu 
       puts <<-MENU
-      1. List Books
-      2. List Houses
+      1. List All Books
+      2. List 50 Houses
       Enter 'exit' to exit program.
       MENU
     end
@@ -52,15 +55,14 @@ module ScrapeGot
         puts "#{index} #{house.name}: #{house.words}"
         puts "Coat of Arms: #{house.coat}"
         puts "Region: #{house.region}"
-        puts "--------------------------------------------------------------------"
+        puts "-------------------------------------------------------------------------".colorize(:light_yellow)
         
       end
     end
     
     def end_program
-      puts "Thanks for checking out I scrape and I know things, see ya soon!"
+      puts "Thanks for checking out I scrape and I know things, see ya soon!".colorize(:light_yellow)
     end
-
 
   end
 end
