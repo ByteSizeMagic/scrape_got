a45aace4ef9b656f43a15473ebc732b9db6a7fdc
ByteSizeMagic <bytesizemagic@gmail.com>
2020-07-17 21:19:08 -0700

    linked info class

--- lib/scrape_got.rb
+++ lib/scrape_got.rb
@@ -1,7 +1,9 @@
-require "pry"
 require_relative "./scrape_got/version"
 require_relative "./scrape_got/cli"
 require_relative "./scrape_got/api"
+require_relative "./scrape_got/info"
+
+
 
 module ScrapeGot
   class Error < StandardError; end
