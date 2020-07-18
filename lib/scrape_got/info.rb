efb3574c83a0645b9e0a499c5f8ab9fa3eec477c
ByteSizeMagic <bytesizemagic@gmail.com>
2020-07-18 14:49:16 -0700

    added houses to info class

--- lib/scrape_got/info.rb
+++ lib/scrape_got/info.rb
@@ -1,11 +1,15 @@
 module ScrapeGot
   class Info
     
-    attr_accessor :name, :numberOfPages, :released, :characters
+    attr_accessor :name, :number_of_pages, :released, :coat, :region, :words
 
 
     def self.all_books
       @@all_books ||= self.load_books
+    end
+
+    def self.all_houses
+      @@all_houses ||= self.load_houses
     end
 
     def self.load_books
@@ -14,20 +18,24 @@ module ScrapeGot
       end
     end
 
+    def self.load_houses
+      API.get_houses.collect do |houses_hash|
+        Info.new(houses_hash)
+      end
+    end
+
     def initialize(attributes = {})
       @name = attributes["name"]
-      @released = attributes["released"]
+      @number_of_pages = attributes["numberOfPages"]
+      @released = attributes["released"]#.strftime("Released on %m/%d/%Y")
+      @coat = attributes["coatOfArms"]
+      @region = attributes["region"]
+      @words = attributes["words"]
     end
     
     # def self.load_characters
 
     # end
-    
-    # def self.load_houses
-
-    # end
-
-   
     
     
   end
