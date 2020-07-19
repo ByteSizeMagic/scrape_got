eea639181713ab554fde43fe1cad1944d30874f9
ByteSizeMagic <bytesizemagic@gmail.com>
2020-07-18 17:36:50 -0700

    added books class

---
+++ lib/scrape_got/books.rb
@@ -0,0 +1,24 @@
+module ScrapeGot
+  class Books
+    
+    attr_accessor :name, :number_of_pages, :released
+
+
+    def self.all_books
+      @@all_books ||= self.load_books
+    end
+
+    def self.load_books
+      API.get_books.collect do |book_hash|
+        Books.new(book_hash)
+      end
+    end
+
+    def initialize(attributes = {})
+      @name = attributes["name"]
+      @number_of_pages = attributes["numberOfPages"]
+      @released = attributes["released"]#.strftime("Released on %m/%d/%Y")
+    end
+    
+  end
+end
