ea8f4b5b51b8191381ec820301a8b70c11549c34
ByteSizeMagic <bytesizemagic@gmail.com>
2020-07-18 23:19:32 -0700

    cleaned up api class

--- lib/scrape_got/api.rb
+++ lib/scrape_got/api.rb
@@ -3,29 +3,18 @@ require 'httparty'
 
 module ScrapeGot
   class API
-    # API_HOST = "https://www.anapioficeandfire.com/api"
-    # BOOK_PATH = "/books?page=1&pageSize=50"
-    # CHARACTER_PATH = "/characters?page=1&pageSize=50"
-    # HOUSE_PATH = "/houses?page=1&pageSize=50"
-
-
 
     def self.get_books
-      response = HTTParty.get("https://www.anapioficeandfire.com/api/books?page=1&pageSize=50")
+      response = HTTParty.get("https://www.anapioficeandfire.com/api/books?page=1&pageSize=20")
       body = response.body
       @data = JSON.parse(body)
     end
 
-    # def get_characters
-    #   response = HTTParty.get("https://www.anapioficeandfire.com/api/characters?page=1&pageSize=50")
-    #   body = response.body
-    #   @data = JSON.parse(body)
-    # end
-    
     def self.get_houses
-      response = HTTParty.get("https://www.anapioficeandfire.com/api/houses?page=1&pageSize=50")
+      response = HTTParty.get("https://www.anapioficeandfire.com/api/houses?page=1&pageSize=20")
       body = response.body
       @data = JSON.parse(body)
     end
+    
   end
 end
