72c701cf9c39495a64056954278d19a572d9eb38
ByteSizeMagic <bytesizemagic@gmail.com>
2020-07-19 13:26:20 -0700

    refactored cli class

--- lib/scrape_got/cli.rb
+++ lib/scrape_got/cli.rb
@@ -2,30 +2,29 @@ require 'colorize'
 
 module ScrapeGot
   class CLI
+    
     def run
       system("clear")
       greeting
       Books.all_books
       Houses.all_houses
-      puts "___________________________________\n".colorize(:light_yellow)
-      puts "Please choose an option: "
+      
       @user_input = nil
       menu
-      while @user_input != "exit"
-        @user_input = gets.strip.downcase
-          if @user_input == "1"
-            puts "List Books:".colorize(:light_yellow)
-            print_books
-            display_menu_message
-          elsif @user_input == "2"
-            puts "List Houses:".colorize(:light_yellow)
-            print_houses
-            display_menu_message
-          else
-            display_menu_message
+        while @user_input != "exit"
+          @user_input = gets.strip.downcase
+            if @user_input == "1"
+              puts "List Books:".colorize(:light_yellow)
+              Books.print_books
+              menu
+            elsif @user_input == "2"
+              puts "List Houses:".colorize(:light_yellow)
+              Houses.print_houses
+              menu
+            else
+              @user_input == "exit" ? break : display_menu_message
+            end
           end
-      end
-      #binding.pry
       end_program
     end
 
@@ -34,35 +33,26 @@ module ScrapeGot
     end
 
     def menu 
+      display_line
+      puts "Please choose an option: "
       puts <<-MENU
       1. List All Books
-      2. List 50 Houses
-      Enter 'exit' to exit program.
+      2. List 20 Houses
       MENU
+      display_menu_message
     end
 
     def display_menu_message
-      puts "Please enter a menu item number or 'exit' to exit the program"
+      puts "Please enter a menu item number or type " + "exit".colorize(:red) + " to exit the program"
     end
 
-    def print_books
-      Books.all_books.each.with_index(1) do |book, index|
-        puts "Book #{index}: #{book.name}, released: #{book.released}"
-      end
-    end
-
-    def print_houses
-      Houses.all_houses.each.with_index(1) do |house, index|
-        puts "#{index} #{house.name}: #{house.words}"
-        puts "Coat of Arms: #{house.coat}"
-        puts "Region: #{house.region}"
-        puts "-------------------------------------------------------------------------".colorize(:light_yellow)
-        
-      end
+    def display_line
+      puts "___________________________________\n".colorize(:light_yellow)
     end
     
     def end_program
       puts "Thanks for checking out I scrape and I know things, see ya soon!".colorize(:light_yellow)
+      display_line
     end
 
   end
